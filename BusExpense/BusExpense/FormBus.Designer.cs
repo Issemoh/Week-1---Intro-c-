383e1d701195b53b3173116e10b935893572ebbd
Ice <your name>
2020-01-28 11:34:45 -0600

    completed

--- BusExpense/BusExpense/FormBus.Designer.cs
+++ BusExpense/BusExpense/FormBus.Designer.cs
@@ -29,16 +29,24 @@
         private void InitializeComponent()
         {
             this.btnCalculate = new System.Windows.Forms.Button();
-            this.txtTrips = new System.Windows.Forms.TextBox();
-            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.txtCreditAdded = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.btnExit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDaysParking = new System.Windows.Forms.TextBox();
+            this.txtCreditRemaining = new System.Windows.Forms.TextBox();
+            this.txtOneParkWithCard = new System.Windows.Forms.TextBox();
+            this.txtOneParkNoCard = new System.Windows.Forms.TextBox();
+            this.txtSavings = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnCalculate
             // 
-            this.btnCalculate.Location = new System.Drawing.Point(473, 155);
+            this.btnCalculate.Location = new System.Drawing.Point(494, 199);
             this.btnCalculate.Name = "btnCalculate";
             this.btnCalculate.Size = new System.Drawing.Size(66, 19);
             this.btnCalculate.TabIndex = 2;
@@ -46,45 +54,38 @@
             this.btnCalculate.UseVisualStyleBackColor = true;
             this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
             // 
-            // txtTrips
+            // txtCreditAdded
             // 
-            this.txtTrips.Location = new System.Drawing.Point(473, 86);
-            this.txtTrips.Name = "txtTrips";
-            this.txtTrips.Size = new System.Drawing.Size(100, 20);
-            this.txtTrips.TabIndex = 3;
-            // 
-            // txtTotal
-            // 
-            this.txtTotal.Location = new System.Drawing.Point(473, 116);
-            this.txtTotal.Name = "txtTotal";
-            this.txtTotal.ReadOnly = true;
-            this.txtTotal.Size = new System.Drawing.Size(100, 20);
-            this.txtTotal.TabIndex = 4;
+            this.txtCreditAdded.Location = new System.Drawing.Point(484, 70);
+            this.txtCreditAdded.Name = "txtCreditAdded";
+            this.txtCreditAdded.Size = new System.Drawing.Size(100, 20);
+            this.txtCreditAdded.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(323, 86);
+            this.label1.Location = new System.Drawing.Point(232, 73);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(132, 17);
+            this.label1.Size = new System.Drawing.Size(194, 17);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Number of bus trips";
+            this.label1.Text = "Credit added to Campus Card";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(323, 119);
+            this.label2.Location = new System.Drawing.Point(232, 116);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.Size = new System.Drawing.Size(223, 17);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Total spent";
+            this.label2.Text = "Parking cost without Campus Card";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(473, 197);
+            this.btnExit.Location = new System.Drawing.Point(494, 364);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
             this.btnExit.TabIndex = 7;
@@ -92,20 +93,108 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(232, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Parking cost with Campus Card";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(235, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Number of days of parking ";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(235, 286);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Credit remaining";
+            // 
+            // txtDaysParking
+            // 
+            this.txtDaysParking.Location = new System.Drawing.Point(484, 243);
+            this.txtDaysParking.Name = "txtDaysParking";
+            this.txtDaysParking.ReadOnly = true;
+            this.txtDaysParking.Size = new System.Drawing.Size(100, 20);
+            this.txtDaysParking.TabIndex = 11;
+            // 
+            // txtCreditRemaining
+            // 
+            this.txtCreditRemaining.Location = new System.Drawing.Point(484, 286);
+            this.txtCreditRemaining.Name = "txtCreditRemaining";
+            this.txtCreditRemaining.ReadOnly = true;
+            this.txtCreditRemaining.Size = new System.Drawing.Size(100, 20);
+            this.txtCreditRemaining.TabIndex = 12;
+            // 
+            // txtOneParkWithCard
+            // 
+            this.txtOneParkWithCard.Location = new System.Drawing.Point(484, 161);
+            this.txtOneParkWithCard.Name = "txtOneParkWithCard";
+            this.txtOneParkWithCard.Size = new System.Drawing.Size(100, 20);
+            this.txtOneParkWithCard.TabIndex = 13;
+            // 
+            // txtOneParkNoCard
+            // 
+            this.txtOneParkNoCard.Location = new System.Drawing.Point(484, 115);
+            this.txtOneParkNoCard.Name = "txtOneParkNoCard";
+            this.txtOneParkNoCard.Size = new System.Drawing.Size(100, 20);
+            this.txtOneParkNoCard.TabIndex = 14;
+            // 
+            // txtSavings
+            // 
+            this.txtSavings.Location = new System.Drawing.Point(484, 328);
+            this.txtSavings.Name = "txtSavings";
+            this.txtSavings.ReadOnly = true;
+            this.txtSavings.Size = new System.Drawing.Size(100, 20);
+            this.txtSavings.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(239, 328);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(175, 17);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Savings with Campus Card";
+            this.label6.Click += new System.EventHandler(this.label6_Click_1);
+            // 
             // FormBus
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(704, 399);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtSavings);
+            this.Controls.Add(this.txtOneParkNoCard);
+            this.Controls.Add(this.txtOneParkWithCard);
+            this.Controls.Add(this.txtCreditRemaining);
+            this.Controls.Add(this.txtDaysParking);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtTotal);
-            this.Controls.Add(this.txtTrips);
+            this.Controls.Add(this.txtCreditAdded);
             this.Controls.Add(this.btnCalculate);
             this.Name = "FormBus";
-            this.Text = "Bus Expenses";
+            this.Text = "Parking Card Helper";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -115,11 +204,19 @@
         #endregion
 
         private System.Windows.Forms.Button btnCalculate;
-        private System.Windows.Forms.TextBox txtTrips;
-        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtCreditAdded;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDaysParking;
+        private System.Windows.Forms.TextBox txtCreditRemaining;
+        private System.Windows.Forms.TextBox txtOneParkWithCard;
+        private System.Windows.Forms.TextBox txtOneParkNoCard;
+        private System.Windows.Forms.TextBox txtSavings;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- BusExpense/BusExpense/FormBus.cs
+++ BusExpense/BusExpense/FormBus.cs
@@ -24,28 +24,61 @@ namespace BusExpense
 
         private void Form1_Load(object sender, EventArgs e)
         {
-          
+
         }
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
-            double busFare = 2.0;
 
-            int trips;
+            bool creditValid = Double.TryParse(txtCreditAdded.Text, out double creditAdded); //inline variable creation
+            bool costOneParkWithCardValid = Double.TryParse(txtOneParkWithCard.Text, out double costOneParkWithCard);
+            bool costOneParkNoCardValid = Double.TryParse(txtOneParkNoCard.Text, out double costOneparkNoCard);
 
-            if (Int32.TryParse(txtTrips.Text, out trips)){
-                double total = busFare * trips;
-                txtTotal.Text = "$" + total;
+            if (creditValid && costOneParkWithCardValid && costOneParkNoCardValid)
+            {
+                int daysParking = (int)(creditAdded / costOneParkWithCard);
+                double creditRemaining = creditAdded % costOneParkWithCard;
+                double oneDaySavings = costOneparkNoCard - costOneParkWithCard;
+                double totalSavings = daysParking * oneDaySavings;
+
+                txtDaysParking.Text = daysParking.ToString();
+                txtCreditRemaining.Text = creditRemaining.ToString("c");
+                txtSavings.Text = totalSavings.ToString("c");
             }
             else
             {
-                MessageBox.Show("Enter only numbers", "Error");
+                MessageBox.Show("Please enter numbers", "Error");
             }
         }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTotal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
